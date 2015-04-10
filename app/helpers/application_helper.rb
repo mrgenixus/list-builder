7a9edd4ca06890ae33e4a7adcf26b0b17820c703
Benjamin West <mrgenixus@gmail.com>
2015-04-09 22:46:56 -0600

    Improved Session UI

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
   def quote str
     ('"' + str.gsub(/\"/, "\"") + '"').html_safe
   end
+
+   def avatar_url(user, size = 64)
+      default_url = "#{root_url}images/guest.png"
+      gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+      "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&d=#{CGI.escape(default_url)}"
+    end
 end
