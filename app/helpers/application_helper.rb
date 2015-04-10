b23cc74d23fde0b076bb76db21760c8bc00a9e85
Benjamin West <mrgenixus@gmail.com>
2015-04-09 22:57:40 -0600

    Fix somethings

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,9 +3,9 @@ module ApplicationHelper
     ('"' + str.gsub(/\"/, "\"") + '"').html_safe
   end
 
-   def avatar_url(user, size = 64)
-      default_url = "#{root_url}images/guest.png"
-      gravatar_id = Digest::MD5::hexdigest(user.email).downcase
-      "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&d=#{CGI.escape(default_url)}"
-    end
+  def avatar_url(user, size = 64)
+    default_url = "#{root_url}images/guest.png"
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=#{size}&d=#{CGI.escape(default_url)}"
+  end
 end
