2f6417c377d439cf25923be78cb53421f07d0e0d
Benjamin West <mrgenixus@gmail.com>
2015-04-14 22:01:41 -0600

    Updates

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
     resources :memberships, only: [:create, :destroy, :show]
   end
   resources :people
-  resource :contact_form, controller: :contact_form
+  resource :contact_form, controller: :contact_forms
   get '/:id/thank_you', to: 'contact_forms#thank_you', as: :sign_up_thank_you
 
   root to: "lists#index"
